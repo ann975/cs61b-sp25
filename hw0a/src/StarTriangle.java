aaa9eb707701147b4853d31b1beebc190e13ca87
ann975 <annsangeuncho@gmail.com>
2026-01-04 14:51:38 -0500

    StarTriangle finish

--- hw0a/src/StarTriangle.java
+++ hw0a/src/StarTriangle.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class StarTriangle {
+
+    public static void printStars(int size){
+        int row = 0;
+        while(row < size){
+            int col = 0;
+            while(col <= row){
+                System.out.print("* ");
+                col++;
+
+            }
+            System.out.println();
+            row++;
+        }
+
+    }
+
+    public static void main(String[]args){
+        printStars(5);
+    }
 }
